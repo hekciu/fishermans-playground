dfb5a2570e0fb0211d052ca601d9bece3234a0c3
Jakub Lasecki <jakublasecki321@gmail.com>
2024-05-03 16:41:30 +0000

    a bit of button rendering implementation

--- engine/game_objects.h
+++ engine/game_objects.h
@@ -48,17 +48,24 @@ namespace GameObjects {
         int width;
         int height;
         Color color;
-        RectangleButton2D(int posX, int posY, int width, int height, Color color, std::string text = NULL) {
+        RectangleButton2D(int posX, int posY, int width, int height, Color color, std::string text = NULL, int fontSize = 0) {
             this->posX = posX;
             this->posY = posY;
             this->width = width;
             this->height = height;
             this->color = color;
+            this->text = text;
+            this->fontSize = fontSize;            
         }
 
         ~RectangleButton2D() {};
         void draw() {
             DrawRectangle(this->posX, this->posY, this->width, this->height, this->color);
+            if (this->text != NULL) {
+                int textPosX = this->posX + (this->width - this->fontSize) / 2;
+                int textPosY = this->posY + (this->height - this->fontSize) / 2; // TODO: wycentrowanie tego?
+                DrawText(text.c_str(), )
+            }
         }
     };
 }
